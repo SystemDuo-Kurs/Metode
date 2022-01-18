3ba44b60b50a8c30fe028e2bff7cfaa518301881
Dragan Pepic <nedimco2006@gmail.com>
2022-01-18 17:51:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿public partial class Program
+{
+    public static void Foo(string nesto)
+    {
+        Console.WriteLine(nesto);
+    }
+
+    public static void Main()
+    {
+        int broj = 8;
+        Console.WriteLine($"Broj je {broj} pre poziva");
+        broj = Inc(broj);
+        Console.WriteLine($"Broj je {broj} posle poziva");
+        //Sabirac(5, 6);
+    }
+
+    public static int Inc(int zklj)
+    {
+        Console.WriteLine($"Broj je {zklj} po pozivu");
+        zklj++;
+        Console.WriteLine($"Broj je {zklj} posle uvecavanja");
+        if (zklj % 2 == 0)
+        {
+            return zklj;
+        }
+        else
+        {
+            return zklj * 2;
+        }
+    }
+
+    public static void SabiracJosNesto(int x, int y)
+    {
+        Console.WriteLine("Zbir je: " + (x + y));
+        Console.WriteLine(x + " + " + y + " je: " + (x + y));
+        Console.WriteLine($"{x} + {y} je: {x + y}");
+    }
+}
